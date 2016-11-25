42a45c072ca2b72bbc5d194831bc5492124218c0
fabiano1 <fabiano.ferreira@dce.ufpb.br>
2016-11-24 22:12:59 -0300

    Exercicio Coleçoes

--- POO/src/exer3/Paciente.java
+++ POO/src/exer3/Paciente.java
@@ -1,5 +1,146 @@
 package exer3;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Paciente {
+
+
+	private Endereco endereco;
+	private String sexo;
+	private String nome;
+	private LocalDate dataNasc;
+
+
+
+
+	/**
+	 * @param endereco
+	 * @param sexo
+	 * @param nome
+	 * @param dataNasc
+	 * @param documento
+	 */
+	public Paciente() {
+		this.endereco = null;
+		this.sexo = "";
+		this.nome = "";
+		this.dataNasc = null;
+		this.documento = "";
+	}
+
+	public Paciente(Endereco endereco, String sexo, String nome, LocalDate data, String documento) {
+		this.endereco = endereco;
+		this.sexo = sexo;
+		this.nome = nome;
+		this.dataNasc = data;
+		this.documento = documento;
+	}
+
+
+	public String nomeFormatado(){
+
+		String[] parts = this.nome.split(" ");
+		String nome = new String();
+		String nome1= new String();
+		for(int i=0;i<parts.length;i++){
+			nome=parts[parts.length-1]+", ";
+
+			nome1+=parts[i].substring(0,1)+". ";
+
+		}
+
+	return nome+nome1 ;	
+
+}
+
+
+
+public int idade(){
+
+
+	LocalDate dataHj= LocalDate.now();
+
+	Period p=Period.between(this.dataNasc, dataHj);
+	System.out.println(p.getYears());
+
+
+	return p.getYears();
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Paciente other = (Paciente) obj;
+	if (dataNasc == null) {
+		if (other.dataNasc != null)
+			return false;
+	} else if (!dataNasc.equals(other.dataNasc))
+		return false;
+	if (documento == null) {
+		if (other.documento != null)
+			return false;
+	} else if (!documento.equals(other.documento))
+		return false;
+	if (endereco == null) {
+		if (other.endereco != null)
+			return false;
+	} else if (!endereco.equals(other.endereco))
+		return false;
+	if (nome == null) {
+		if (other.nome != null)
+			return false;
+	} else if (!nome.equals(other.nome))
+		return false;
+	if (sexo == null) {
+		if (other.sexo != null)
+			return false;
+	} else if (!sexo.equals(other.sexo))
+		return false;
+	return true;
+}
+@Override
+public String toString() {
+	return "Paciente [endereco=" + endereco.toString() + ", sexo=" + sexo + ", nome=" + nome + ", data=" + dataNasc
+			+ ", documento=" + documento + "]\n";
+}
+private String documento;
+public String getDocumento() {
+	return documento;
+}
+public void setDocumento(String documento) {
+	this.documento = documento;
+}
+public Endereco getEndereco() {
+	return endereco;
+}
+public void setEndereco(Endereco endereco) {
+	this.endereco = endereco;
+}
+public String getSexo() {
+	return sexo;
+}
+public void setSexo(String sexo) {
+	this.sexo = sexo;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public LocalDate getData() {
+	return dataNasc;
+}
+public void setData(LocalDate data) {
+	this.dataNasc = data;
+}
+
+
+
 
 }
