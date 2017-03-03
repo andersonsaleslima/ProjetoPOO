afbf9f8653f942886781565307dbe98072c91634
andersonsaleslima <a.sales.lima97@gmail.com>
2017-03-02 21:03:14 -0300

    mudanças na impletação da colecaoVeiculo e ColecaoPessoa

--- Rent_A_Car/src/controller/ColecaoPessoa.java
+++ Rent_A_Car/src/controller/ColecaoPessoa.java
@@ -7,99 +7,62 @@ import java.util.ArrayList;
 
 
 public class ColecaoPessoa {
-	private  List<Pessoa> func = new ArrayList<Pessoa>();
-	private List<Pessoa> client =  new ArrayList<Pessoa>();
+	private  List<Pessoa> pessoa = new ArrayList<Pessoa>();
 	
-	public boolean adicionarFuncionario(String nome, Endereco endereco, String cpf, String login, String senha, LocalDate dataNascimento,
-			String cargo, String matricula) {
-			if(cpf != null){
+	public boolean adicionarPessoa(Pessoa pessoa) {
+			if(pessoa != null){
 			
-				this.func.add(new Funcionario(nome,endereco,cpf, login, senha, dataNascimento, cargo, matricula) );
+				this.pessoa.add(pessoa);
 				return true;
 			}
 			return false;
 	}
 
-
-	public boolean adicionarCliente(String nome, Endereco endereco, String cpf, String login, String senha, LocalDate dataNascimento,
-			String cnh, String categoriaCnh) {
-			if(cpf != null){
-			
-				this.client.add(new Cliente(nome,endereco,cpf, login, senha, dataNascimento, cnh, categoriaCnh) );
-				return true;
-			}
-			return false;
-	}
-
-	public Funcionario pesquisaClientePeloCPF(String cpf){
+	public Pessoa pesquisaPessoaPeloCPF(String cpf){
 		int i;
-		Funcionario func;
 	
-		for( i = 0; i < this.func.size(); i++){
-			func = (Funcionario) this.func.get(i);
-			if(func.getCpf().equals(cpf)){
+		for( i = 0; i < this.pessoa.size(); i++){
+			if(this.pessoa.get(i).getCpf().equals(cpf)){
 				//System.out.println(this.paciente.get(i));
-				return func;
-			}
-		}
-		return null;
-	}
-
-	public Cliente pesquisaFuncioanrioPeloCPF(String cpf){
-		int i;
-		Cliente client;
-	
-		for( i = 0; i < this.client.size(); i++){
-			client = (Cliente) this.client.get(i);
-			if(client.getCpf().equals(cpf)){
-				//System.out.println(this.paciente.get(i));
-				return client;
+				return this.pessoa.get(i);
 			}
 		}
 		return null;
 	}
 	
-	public boolean removeFuncioanrioPeloCPF(String cpf){
+	public boolean removePessoaPeloCPF(String cpf){
 		int i;
-		for( i = 0; i < this.func.size(); i++){
-			Funcionario func = (Funcionario) this.func.get(i);
-			if(func.getCpf().equals(cpf)){
-				this.func.remove(i);
+		for( i = 0; i < this.pessoa.size(); i++){
+			if(this.pessoa.get(i).getCpf().equals(cpf)){
+				this.pessoa.remove(i);
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	public boolean removeClientePeloCPF(String cpf){
-		int i;
-		for( i = 0; i < this.client.size(); i++){
-			Cliente client = (Cliente) this.client.get(i);
-			if(client.getCpf().equals(cpf)){
-				this.client.remove(i);
-				return true;
-			}
-		}
-		return false;
-	}
 
 	public void listagemCliente(){
-		Cliente client ;
+		Cliente client = null ;
 		
 		System.out.println();
-		for(int i = 0; i< this.client.size(); i++){
-			client = (Cliente) this.client.get(i);
-			System.out.println(client);
+		for(int i = 0; i< this.pessoa.size(); i++){
+			if(client.getClass() == this.pessoa.getClass()){
+				client = (Cliente) this.pessoa.get(i);
+				System.out.println(client);
+			}
 		}
 	}
 	
 	public void listagemFuncionario(){
-		Funcionario func ;
+		Funcionario func = null;
 		
 		System.out.println();
-		for(int i = 0; i< this.func.size(); i++){
-			func = (Funcionario) this.func.get(i);
-			System.out.println(func);
+		for(int i = 0; i< this.pessoa.size(); i++){
+			if(func.getClass() == this.pessoa.getClass()){
+				func = (Funcionario) this.pessoa.get(i);
+				System.out.println(func);
+			}
 		}
 	}
 }
--- Rent_A_Car/src/controller/ColecaoVeiculo.java
+++ Rent_A_Car/src/controller/ColecaoVeiculo.java
@@ -6,103 +6,67 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class ColecaoVeiculo {
-	private List<Veiculo> auto = new ArrayList<Veiculo>();
-	private List<Veiculo> moto =  new ArrayList<Veiculo>();
+	private List<Veiculo> veiculo = new ArrayList<Veiculo>();
 	
-	public boolean adicionarAutomovel(Placa placa, String marca, String modelo, double precoPorHra,
-			int qtdDePortas, int numeroDePassageiros) {
+	public boolean adicionarAutomovel(Veiculo veiculo) {
 		
-			if((placa != null )){
+			if((veiculo != null )){
 			
-				this.auto.add(new Automovel(placa, marca, modelo, precoPorHra, qtdDePortas,
-						numeroDePassageiros) );
+				this.veiculo.add(veiculo );
 				return true;
 			}
 			return false;
 	}
 	
-	public boolean adicionarMotocicleta(Placa placa, String marca, String modelo, double precoPorHra,
-			String estilo) {
-			if(placa != null){
-			
-				this.moto.add(new Motocicleta(placa, marca, modelo, precoPorHra, estilo) );
-				return true;
-			}
-			return false;
-	}
 
-	public Automovel pesquisaAutomovelPelaPlaca(String numerosEletras){
+	public Veiculo pesquisaAutomovelPelaPlaca(String numerosEletras){
 		int i;
-		Automovel auto;
 	
-		for( i = 0; i < this.auto.size(); i++){
-			auto = (Automovel) this.auto.get(i);
-			if(auto.getPlaca().getNumerosEletras().equals(numerosEletras)){
+		for( i = 0; i < this.veiculo.size(); i++){
+
+			if(this.veiculo.get(i).getPlaca().getNumerosEletras().equals(numerosEletras)){
 				//System.out.println(this.paciente.get(i));
-				return auto;
+				return this.veiculo.get(i);
 			}
 		}
 		return null;
 	}
 	
-	public Motocicleta pesquisaMotocicletaPelaPlaca(String numerosEletras){
-		int i;
-		Motocicleta moto;
-	
-		for( i = 0; i < this.auto.size(); i++){
-			moto = (Motocicleta) this.moto.get(i);
-			if(moto.getPlaca().getNumerosEletras().equals(numerosEletras)){
-				//System.out.println(this.paciente.get(i));
-				return moto;
-			}
-		}
-		return null;
-	}
 
-
-	public boolean removeAutomovelPelaPlaca(String numerosEletras){
+	public boolean removeVeiculoPelaPlaca(String numerosEletras){
 		int i;
-		for( i = 0; i < this.auto.size(); i++){
-			Automovel auto = (Automovel) this.auto.get(i);
-			if(auto.getPlaca().getNumerosEletras().equals(numerosEletras)){
-				this.auto.remove(i);
+		for( i = 0; i < this.veiculo.size(); i++){
+			if(this.veiculo.get(i).getPlaca().getNumerosEletras().equals(numerosEletras)){
+				this.veiculo.remove(i);
 				return true;
 			}
 		}
 		return false;
 	}
-	
-	public boolean removeMotocicletaPelaPlaca(String numerosEletras){
-		int i;
-		for( i = 0; i < this.moto.size(); i++){
-			Motocicleta moto = (Motocicleta) this.moto.get(i);
-			if(moto.getPlaca().getNumerosEletras().equals(numerosEletras)){
-				this.moto.remove(i);
-				return true;
-			}
-		}
-		return false;
-	}
-	
+		
 
 	public void listagemAutomovel(){
-		Automovel auto;
+		Automovel auto = null;
 		
 		System.out.println();
-		for(int i = 0; i< this.auto.size(); i++){
-			auto = (Automovel) this.auto.get(i);
-			System.out.println(auto);
+		for(int i = 0; i< this.veiculo.size(); i++){
+			if(auto.getClass() == this.veiculo.getClass()){
+				auto = (Automovel) this.veiculo.get(i);
+				System.out.println(auto);
+			}
 		}
 	}
 	
 	
 	public void listagemMotocicleta(){
-		Motocicleta moto;
+		Motocicleta moto = null;
 		
 		System.out.println();
-		for(int i = 0; i< this.moto.size(); i++){
-			moto = (Motocicleta) this.moto.get(i);
-			System.out.println(moto);
+		for(int i = 0; i< this.veiculo.size(); i++){
+			if(moto.getClass()== this.veiculo.getClass()){				
+				moto = (Motocicleta) this.veiculo.get(i);
+				System.out.println(moto);
+			}
 		}
 	}
 	
